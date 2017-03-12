f40172b0ca2d4ea60b3efcbd3147a4a4cc3abc44
akai-tsuki <akai.tsuki.mobile@gmail.com>
2017-03-13 02:19:33 +0900

    sample go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"testapp/mymath"
+)
+
+func main() {
+	fmt.Print("Hello, world.\n")
+	fmt.Printf("Hello, world.  Sqrt(2) = %v\n", mymath.Sqrt(2))
+}
---
+++ mymath/sqrt.go
@@ -0,0 +1,13 @@
+package mymath
+
+/*
+Sqrt ...
+  test message
+*/
+func Sqrt(x float64) float64 {
+	z := 0.0
+	for i := 0; i < 1000; i++ {
+		z -= (z*z - x) / (2 * x)
+	}
+	return z
+}
